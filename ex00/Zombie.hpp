b08661e50d9e1397f27960e42bfd3b062b9b1006
solareenlo <solareenlo@gmail.com>
2021-11-10 16:37:23 +0900

    Fix: ex00

--- ex00/Zombie.hpp
+++ ex00/Zombie.hpp
@@ -6,7 +6,7 @@
 /*   By: tayamamo <tayamamo@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/10 11:43:52 by tayamamo          #+#    #+#             */
-/*   Updated: 2021/11/10 16:32:00 by tayamamo         ###   ########.fr       */
+/*   Updated: 2021/11/10 16:37:16 by tayamamo         ###   ########.fr       */
 /*   Copyright 2021                                                           */
 /* ************************************************************************** */
 
@@ -18,8 +18,8 @@
 
 class Zombie {
  public:
-    Zombie(std::string name);
     Zombie();
+    Zombie(std::string name);
     ~Zombie();
 
     void announce(void) const;
