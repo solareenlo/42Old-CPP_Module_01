256499f4e26ffaba530b9990435bcb2d13d255f5
solareenlo <solareenlo@gmail.com>
2021-11-11 12:20:48 +0900

    Fix: ex05

--- ex05/Karen.cpp
+++ ex05/Karen.cpp
@@ -6,7 +6,7 @@
 /*   By: tayamamo <tayamamo@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/11 10:16:45 by tayamamo          #+#    #+#             */
-/*   Updated: 2021/11/11 11:22:51 by tayamamo         ###   ########.fr       */
+/*   Updated: 2021/11/11 12:18:51 by tayamamo         ###   ########.fr       */
 /*   Copyright 2021                                                           */
 /* ************************************************************************** */
 
@@ -16,31 +16,44 @@ Karen::Karen() {}
 Karen::~Karen() {}
 
 void Karen::debug(void) {
+    std::cout << "[ DEBUG ]" << std::endl;
     std::cout << "I love to get extra bacon for my "
                  "7XL-double-cheese-triple-pickle-special-ketchup burger. I "
                  "just love it!"
               << std::endl;
+    std::cout << std::endl;
 }
 
 void Karen::info(void) {
+    std::cout << "[ INFO ]" << std::endl;
     std::cout << "I cannot believe adding extra bacon cost more money. You "
                  "don’t put enough! If you did I would not have to ask for it!"
               << std::endl;
+    std::cout << std::endl;
 }
 
 void Karen::warning(void) {
+    std::cout << "[ WARNING ]" << std::endl;
     std::cout
         << "I think I deserve to have some extra bacon for free. I’ve been "
            "coming here for years and you just started working here last month."
         << std::endl;
+    std::cout << std::endl;
 }
 
 void Karen::error(void) {
+    std::cout << "[ ERROR ]" << std::endl;
     std::cout << "This is unacceptable, I want to speak to the manager now."
               << std::endl;
+    std::cout << std::endl;
 }
 
-void Karen::unknown(void) { std::cout << "Unknown level" << std::endl; }
+void Karen::unknown(void) {
+    std::cout << "[ UNKNOWN ]" << std::endl;
+    std::cout << "[ Probably complaining about insignificant problems ]"
+              << std::endl;
+    std::cout << std::endl;
+}
 
 void Karen::complain(std::string level) {
     void (Karen::*funcptr[5])(void) = {
@@ -53,5 +66,6 @@ void Karen::complain(std::string level) {
     for (; i < 4 && str[i] != level;) {
         i++;
     }
+
     (this->*funcptr[i])();
 }
